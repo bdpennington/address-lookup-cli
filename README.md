6f82aad404e5e054e182667a67b3552e53c32ff2
Brian Pennington <bdpennington@gmail.com>
2023-04-18 14:22:56 -0400

    Got package setup complete to run as installable CLI

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Empora Work Sample
+
+## Install
+
+Install deps
+
+```bash
+pnpm install
+```
+
+## Build
+
+```bash
+pnpm run prepare
+```
+
+## Install
+
+You can install this CLI application globally on your system with from the root directory of this project.
+
+```bash
+npm install -g
+```
+
+Or, if you don't want to install it system-wide, you can just run it with node.
+
+```bash
+node .bin/index.js
+```
+
+## Requirements
+
+Because this CLI makes use of node's native fetch API, it requires node 10 or higher.
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,24 @@
+#!/usr/bin/env node
+
 import * as dotenv from 'dotenv';
+import { program } from 'commander';
+
 dotenv.config();
+console.log('env', process.env);
+
+program
+  .name('address-lookup')
+  .version('1.0.0', '-v, --version', 'output the current version')
+  .description('A CLI for fetching addresses from Smarty Address Lookup API')
+  .option('-i --in-file', 'input csv file')
+  .option('-o, --out-file', 'output file');
+
+program.parse();
+
+const run = async () => {
+  console.log('argv', process.argv);
+  await program.parseAsync(process.argv);
+  process.exit(0);
+};
+
+run();
